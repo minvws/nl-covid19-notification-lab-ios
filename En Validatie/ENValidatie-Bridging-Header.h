ca0a3f75b0470b92d06287d717c1def4bb76f92a
Roel Spruit <roelspruit@gmail.com>
2020-12-22 08:49:15 +0100

    iOS 12 compatible QR Code generation

---
+++ En Validatie/ENValidatie-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ENValidatie-Bridging-Header.h
+//  En Validatie
+//
+//  Created by Roel Spruit on 22/12/2020.
+//  Copyright © 2020 Rijksoverheid. All rights reserved.
+//
+
+#ifndef ENValidatie_Bridging_Header_h
+#define ENValidatie_Bridging_Header_h
+
+#import "QRCodeGenerator.h"
+
+#endif /* ENValidatie_Bridging_Header_h */
--- En Validatie/VC/SenderViewController.swift
+++ En Validatie/VC/SenderViewController.swift
@@ -106,16 +106,12 @@ class SenderViewController: UIViewController, UITextFieldDelegate {
     }
     
     private func generateQrCode(code: String) {
-        let data = code.data(using: String.Encoding.ascii)
         
-        if let filter = CIFilter(name: "CIQRCodeGenerator") {
-            filter.setValue(data, forKey: "inputMessage")
-            let transform = CGAffineTransform(scaleX: 10, y: 10)
-            
-            if let output = filter.outputImage?.transformed(by: transform) {
-                imageViewQr.image = UIImage(ciImage: output)
-            }
+        guard let qrCodeImage = QRCodeGenerator(string: code)?.getImage() else {
+            return
         }
+        
+        imageViewQr.image = qrCodeImage
     }
     
 }
